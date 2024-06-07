469d680aef94b01245d8e6850d85725bce0eb2d5
rayliu7717 <rayliu2004@gmail.com>
2024-06-07 09:54:24 -0400

    add mongodb connection, /app/users to get data from mongodb

---
+++ src/people.js
@@ -0,0 +1,10 @@
+export let people = [{
+    name:"john",
+    age:34,
+},{
+    name:"Joe",
+    age:20,
+},{
+    name:"Jane",
+    age:30,
+}]
--- src/server.js
+++ src/server.js
@@ -1,8 +1,66 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import {promises as fs} from 'fs'
+
+import {people} from './people'
+
+
+//const { MongoClient, ServerApiVersion } = require('mongodb');
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = "mongodb+srv://liugeyang:gerry@myclusterprovider.mhfbhk0.mongodb.net/?retryWrites=true&w=majority&appName=myClusterProvider";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+
 let app = express();
+app.use(bodyParser.json())
 
 app.get('/hello', (req,res)=>{
     res.send("Hello!")
+})
+
+app.get('/people', (req,res)=>{
+    res.json(people)
+})
+
+app.get('/people/:name',  (req,res)=>{
+    let {name}=req.params
+    console.log(name)
+    let person = people.find( x=> x.name.toLocaleLowerCase()===name.toLocaleLowerCase())
+    res.json(person)
+})
+
+app.get("/file-data", async (req,res)=>{
+    let data=await fs.readFile(__dirname + '/people-data.json')
+    let people = JSON.parse(data)
+    res.json(people)
+})
+
+app.post('/people', (req,res)=>{
+    let newPerson = req.body;
+    people.push(newPerson)
+    res.json(people)
+
+})
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+app.get('/api/users', async (req,res)=>{
+    try {
+        await client.connect();
+        const users = await client.db("users").collection('users').find({}).toArray();
+        res.send(users)    
+    } 
+    finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 })
 
 app.listen(3000, ()=>{
